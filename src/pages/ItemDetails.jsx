c891cf6042a206d77b108a49edfefdb13ff01d9d
StephanieGL <stephanie.gl@icloud.com>
2025-07-16 00:58:34 -0400

    Items Details page updated

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -2,42 +2,39 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import Skeleton from "../components/UI/Skeleton";
-import useCountdown from "../components/hooks/useCountdown";
+import Countdown from "../components/UI/Countdown";
+import eth from "../images/ethereum.svg";
 
 const ItemDetails = () => {
   const { nftId } = useParams();
-  const [nft, setNft] = useState(null);
+  const [item, setItem] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  async function fetchNftDetails() {
-    setLoading(true);
-    try {
-      const { data } = await axios.get(
-        `https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${nftId}`
-      );
-      setNft(data);
-    } catch (error) {
-      console.error("Error fetching NFT details:", error);
-      setNft(null); // Ensure nft is null on error
-    } finally {
-      setLoading(false);
-    }
-  }
-
   useEffect(() => {
+    const fetchItemDetails = async () => {
+      setLoading(true);
+      try {
+        const { data } = await axios.get(
+          `https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${nftId}`,
+          { timeout: 5000 } // <-- Add a 5-second timeout
+        );
+        setItem(data);
+      } catch (error) {
+        console.error("API Request Failed:", error);
+        setItem(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
     window.scrollTo(0, 0);
-    fetchNftDetails();
+    fetchItemDetails();
   }, [nftId]);
 
-  const Countdown = ({ expiryDate }) => {
-    const { days, hours, minutes, seconds } = useCountdown(expiryDate);
-    return <div className="de_countdown">{`${days}d ${hours}h ${minutes}m ${seconds}s`}</div>;
-  };
-  
   const renderSkeleton = () => (
     <div className="row">
       <div className="col-md-6 text-center">
-        <Skeleton width="100%" height="500px" />
+        <Skeleton width="100%" height="100%" borderRadius="8px" />
       </div>
       <div className="col-md-6">
         <div className="item_info">
@@ -45,15 +42,6 @@ const ItemDetails = () => {
           <div className="item_info_counts">
             <Skeleton width="80px" height="30px" />
             <Skeleton width="80px" height="30px" />
-          </div>
-          <p><Skeleton width="100%" height="100px" /></p>
-          <div className="item_author">
-            <div className="author_list_pp">
-              <Skeleton width="50px" height="50px" borderRadius="50%" />
-            </div>
-            <div className="author_list_info">
-              <Skeleton width="120px" height="20px" />
-            </div>
           </div>
         </div>
       </div>
@@ -66,76 +54,70 @@ const ItemDetails = () => {
         <div id="top"></div>
         <section aria-label="section" className="mt90 sm-mt-0">
           <div className="container">
-            {/* THIS IS THE KEY FIX: We check for loading OR if nft is null */}
-            {loading || !nft ? renderSkeleton() : (
+            {loading ? (
+              renderSkeleton()
+            ) : item ? (
               <div className="row">
                 <div className="col-md-6 text-center">
-                  {nft.expiryDate && <Countdown expiryDate={nft.expiryDate} />}
                   <img
-                    src={nft.nftImage}
+                    src={item.nftImage}
                     className="img-fluid img-rounded mb-sm-30"
-                    alt={nft.title}
+                    alt=""
                   />
                 </div>
                 <div className="col-md-6">
                   <div className="item_info">
-                    <h2>{nft.title} #{nft.tag}</h2>
+                    {item.expiryDate && <Countdown expiryDate={item.expiryDate} />}
+                    <h2>{item.title} #{item.tag}</h2>
                     <div className="item_info_counts">
                       <div className="item_info_views">
                         <i className="fa fa-eye"></i>
-                        {nft.views}
+                        {item.views}
                       </div>
                       <div className="item_info_like">
                         <i className="fa fa-heart"></i>
-                        {nft.likes}
+                        {item.likes}
                       </div>
                     </div>
-                    <p>{nft.description}</p>
-                    <div className="d-flex flex-row">
-                      <div className="mr40">
-                        <h6>Owner</h6>
-                        <div className="item_author">
-                          <div className="author_list_pp">
-                            <Link to={`/author/${nft.ownerId}`}>
-                              <img className="lazy" src={nft.ownerImage} alt="" />
-                              <i className="fa fa-check"></i>
-                            </Link>
-                          </div>
-                          <div className="author_list_info">
-                            <Link to={`/author/${nft.ownerId}`}>{nft.ownerName}</Link>
-                          </div>
-                        </div>
+                    <p>{item.description}</p>
+                    <h6>Owner</h6>
+                    <div className="item_author">
+                      <div className="author_list_pp">
+                        <Link to={`/author/${item.ownerId}`}>
+                          <img className="lazy" src={item.ownerImage} alt="" />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="author_list_info">
+                        <Link to={`/author/${item.ownerId}`}>{item.ownerName}</Link>
                       </div>
                     </div>
-                    <div className="de_tab tab_simple">
-                      <div className="de_tab_content">
-                        <h6>Creator</h6>
-                        <div className="item_author">
-                          <div className="author_list_pp">
-                            <Link to={`/author/${nft.creatorId}`}>
-                              <img className="lazy" src={nft.creatorImage} alt="" />
-                              <i className="fa fa-check"></i>
-                            </Link>
-                          </div>
-                          <div className="author_list_info">
-                            <Link to={`/author/${nft.creatorId}`}>{nft.creatorName}</Link>
-                          </div>
-                        </div>
+                    <div className="spacer-40"></div>
+                    <h6>Creator</h6>
+                    <div className="item_author">
+                      <div className="author_list_pp">
+                        <Link to={`/author/${item.creatorId}`}>
+                          <img className="lazy" src={item.creatorImage} alt="" />
+                          <i className="fa fa-check"></i>
+                        </Link>
                       </div>
-                      <div className="spacer-30"></div>
-                      <h6>Price</h6>
-                      <div className="nft-item-price">
-                        <img src="/images/misc/ethereum.svg" alt="" />
-                        <span>{nft.price} ETH</span>
+                      <div className="author_list_info">
+                        <Link to={`/author/${item.creatorId}`}>{item.creatorName}</Link>
                       </div>
-                      <div className="d-flex flex-row mt-5">
-                        <button className="btn-main lead m-auto">
-                          Place a bid
-                        </button>
-                      </div>
+                    </div>
+                    <div className="spacer-40"></div>
+                    <h6>Price</h6>
+                    <div className="nft-item-price">
+                      <img src={eth} alt="" />
+                      <span>{item.price}</span>
                     </div>
                   </div>
                 </div>
+              </div>
+            ) : (
+              <div className="text-center">
+                <h2>Could not load item details.</h2>
+                <p>Please check your network connection and try again.</p>
               </div>
             )}
           </div>
