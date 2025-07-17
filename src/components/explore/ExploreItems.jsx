babedc92ddfe1072258b44c009a2a1be2f299a1a
StephanieGL <stephanie.gl@icloud.com>
2025-07-17 14:03:21 -0400

    Bugs Fixed

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -339,6 +339,7 @@ const ExploreItems = () => {
               </div>
             </div>
           ))}
+          // Load more button
       {displayCount < items.length && (
         <div className="col-md-12 text-center">
           <Link
