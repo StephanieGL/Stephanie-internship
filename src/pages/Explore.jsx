a021ee40bd366e7b54d2388d4afd1aac0a132dba
StephanieGL <stephanie.gl@icloud.com>
2025-07-17 12:53:54 -0400

    Loading State Explore Page Updated

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import SubHeader from "../images/subheader.jpg";
 import ExploreItems from "../components/explore/ExploreItems";
 import axios from "axios";
-import Skeleton from "../components/UI/Skeleton";
+import Skeleton from "../components/UI/LoadSkeleton";
 
 const Explore = () => {
   const [exploreItems, setExploreItems] = useState([]);
